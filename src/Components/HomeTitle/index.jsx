03729b0de6e804d6fe6505aeb66e3e80bb1d5ede
SVergnaudExpleo <80250723+SVergnaudExpleo@users.noreply.github.com>
2021-09-30 15:12:41 +0200

    (create) HomeTitle.jsx

---
+++ src/Components/HomeTitle/index.jsx
@@ -0,0 +1,90 @@
+// import react modules //
+import React from 'react'
+// import preject component //
+import colors from '../../Utils/Styles/colors'
+import screenSize from '../../Utils/Styles/breakpoint'
+import homeTitleBg from '../../Assets/homeTitleBg.png'
+// style management //
+function containerStyle(state) {
+    let height = ''
+    let margin =''
+    if (state.isPhoneV === true || state.isPhoneH === true) {
+        height='100px'
+        margin='0 20px 0 20px'
+    }else if (state.isDesktop === true) {
+        height='50px'
+        margin='0 100px 0 100px'
+    }
+    return(
+        {
+            fontSize:height,
+            margin:margin,
+            
+        }
+    )
+}
+function imgStyle(state){
+    return(
+        {
+            borderRadius: '10px',
+            position:'relative',
+            zIndex:'1',
+            height: '100px',
+            width: '100%',
+        }
+    )
+}
+function titleStyle(state){
+    let font = ''
+    if (state.isPhoneV === true || state.isPhoneH === true) {
+        font='50px'
+    }else if (state.isDesktop === true) {
+        font='20px'
+    }
+    return(
+        {
+            position:'absolute',
+            zIndex:'2',
+            fontSize: font,
+        }
+    )
+}
+
+// main component //
+class HomeTitle extends React.Component{
+    constructor(props){
+        super(props)
+        let screenState = new screenSize('400px','800px','2000px')
+        this.state = {
+            isPhoneV: screenState.phoneScreenVerti.matches,
+            isPhoneH: screenState.phoneScreenHori.matches,
+            isDesktop: screenState.desktopScreen.matches
+        }
+        this.updateState = this.updateState.bind(this)
+        screenState.phoneScreenVerti.addEventListener('change',this.updateState)
+        screenState.phoneScreenHori.addEventListener('change',this.updateState)
+        screenState.desktopScreen.addEventListener('change',this.updateState)
+    }
+    
+    updateState() {
+        let screenUpdate = new screenSize('400px','800px','2000px')
+        this.setState({
+            isPhoneV: screenUpdate.phoneScreenVerti.matches,
+            isPhoneH: screenUpdate.phoneScreenHori.matches,
+            isDesktop: screenUpdate.desktopScreen.matches
+        })
+    }
+
+    render() {
+        return (
+            <div style={containerStyle(this.state)}>
+                <div style={titleStyle(this.state)}>
+                    Chez vous, partout et ailleurs
+                </div>
+                <img style={imgStyle(this.state)} src={homeTitleBg} alt='title img'/>
+            </div>
+        )
+    }
+}
+
+export default HomeTitle
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,11 +1,15 @@
+// import react modules //
+import React from "react";
+// import project components //
+import HomeTitle from "../../Components/HomeTitle";
 
+class Home extends React.Component{
 
-function Home() {
-  return (
-    <div className="App">
-      MAISON
-    </div>
-  );
+  render() {
+    return (
+        <HomeTitle />
+    )
+  }
 }
 
-export default Home;
+export default Home
